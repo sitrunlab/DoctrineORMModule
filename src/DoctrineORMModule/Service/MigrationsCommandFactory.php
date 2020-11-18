02fd46ba4e6ff56f857d6190fb7031705c2d2b20
Tom H Anderson <tom.h.anderson@gmail.com>
2020-11-18 04:58:04 -0700

    Removed custom loaders; configure in factory

--- src/DoctrineORMModule/Service/MigrationsCommandFactory.php
+++ src/DoctrineORMModule/Service/MigrationsCommandFactory.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace DoctrineORMModule\Service;
 
+use Doctrine\Migrations\Configuration\Migration\ConfigurationArray;
+use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
 use Doctrine\Migrations\DependencyFactory;
 use Doctrine\Migrations\Tools\Console\Command\DoctrineCommand;
-use DoctrineORMModule\Console\Loader;
 use Interop\Container\ContainerInterface;
 use InvalidArgumentException;
 use Laminas\ServiceManager\FactoryInterface;
 use Laminas\ServiceManager\ServiceLocatorInterface;
+use Symfony\Component\Console\Input\ArgvInput;
 
 use function class_exists;
 use function strtolower;
@@ -23,6 +25,9 @@ class MigrationsCommandFactory implements FactoryInterface
 {
     /** @var string */
     private $name;
+
+    /** @var string */
+    private $defaultObjectManagerName = 'doctrine.entitymanager.orm_default';
 
     public function __construct(string $name)
     {
@@ -45,11 +50,23 @@ class MigrationsCommandFactory implements FactoryInterface
         }
 
         $config = $container->get('config');
+        $objectManagerName = $this->getObjectManagerName();
+
+        // Copied from DoctrineModule/ServiceFactory/AbstractDoctrineServiceFactory
+        if (
+            ! preg_match(
+                '/^doctrine\.((?<mappingType>orm|odm)\.|)(?<serviceType>[a-z0-9_]+)\.(?<serviceName>[a-z0-9_]+)$/',
+                $objectManagerName,
+                $matches
+            )
+        ) {
+            throw new RuntimeException('The object manager alias is invalid: ' . $objectManagerAlias);
+        }
 
         return new $className(
             DependencyFactory::fromEntityManager(
-                new Loader\MigrationsConfiguration($config['doctrine']['migrations_configuration']),
-                new Loader\MigrationsEntityManager($container)
+                new ConfigurationArray($config['doctrine']['migrations_configuration'][$matches['serviceName']]),
+                new ExistingEntityManager($container->get($objectManagerName))
             )
         );
     }
@@ -60,5 +77,16 @@ class MigrationsCommandFactory implements FactoryInterface
     public function createService(ServiceLocatorInterface $container): DoctrineCommand
     {
         return $this($container, 'Doctrine\Migrations\Tools\Console\Command\\' . $this->name . 'Command');
+    }
+
+    private function getObjectManagerName(): string
+    {
+        $arguments = new ArgvInput();
+
+        if (! $arguments->hasParameterOption('--object-manager')) {
+            return $this->defaultObjectManagerName;
+        }
+
+        return $arguments->getParameterOption('--object-manager');
     }
 }
